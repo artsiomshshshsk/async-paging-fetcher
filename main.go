155fdd056b37943433b9f99b843056f7d0b42815
artsiomshshshsk <artsiomshablinskiy@gmail.com>
2023-04-06 19:04:33 +0200

    comments are added

--- main.go
+++ main.go
@@ -73,21 +73,25 @@ func Fetch(url, username, password string) FetchResult {
 
 
 func SpawnFetchers(urlPattern, username, password string, cancel chan struct{}, syncChan chan Future) {
+	// chanel to limit throughput
 	concurrencySemaphore := make(chan struct{}, maxConcurrency)
 	pageId := 1
 	for {
 		select {
 		case <-cancel:
+			// close channel if blank page is reached
 			close(syncChan)
 			return
 		case concurrencySemaphore <- struct{}{}:
 			{
 				url := fmt.Sprintf(urlPattern, pageId)
 				future := make(chan FetchResult)
+				// future is container for not computed value
 				go func() {
 					future <- Fetch(url, username, password)
 					<-concurrencySemaphore
 				}()
+				// chanel to read pages
 				syncChan <- Future{future}
 			}
 		}
@@ -96,14 +100,20 @@ func SpawnFetchers(urlPattern, username, password string, cancel chan struct{}, 
 }
 
 func AsyncFetch(urlPattern, username, password string, callback func([]byte)) {
+	// channel to send futures of FetchResult
 	syncChan := make(chan Future, maxConcurrency)
+
+	// channel to notify SpawnFetchers gourutine to stop spawning 
+	// new goroutines for parsing pages, beacause empty page already reached
 	cancel := make(chan struct{}, maxConcurrency)
 	go SpawnFetchers(urlPattern, username, password, cancel, syncChan)
 
 	for future := range syncChan{
+		// wait untill result is ready
 		curResult := <- future.FetchResult
 		if curResult.err != nil {
 			if errors.Is(curResult.err, PageAfterLastFetchError) {
+				// if there is no content on the page, stop SpawnFetchers goroutine
 				cancel <- struct{}{}
 			} else {
 				panic(curResult.err)
@@ -115,10 +125,14 @@ func AsyncFetch(urlPattern, username, password string, callback func([]byte)) {
 
 func TotalUnique(urlPattern string, username string, password string) int {
 
+	// set for storing events that happend within one day
 	sameDayEvents := make(map[util.Event]bool)
+	// variable to track when day changes, to add totalUnique events and
+	// to release space in the set
 	startingDay := "-"
 	totalUnique := 0
 
+	// async call to parse pages, we pass callback func here,
 	AsyncFetch(urlPattern, username, password, func(page []byte) {
 		lines := strings.Split(strings.TrimSpace(string(page)), "\n")
 
@@ -127,9 +141,11 @@ func TotalUnique(urlPattern string, username string, password string) int {
 			if err != nil {
 				continue
 			}
+			//if current day is equal to previos, we add it to the set
 			if event.Date == startingDay {
 				sameDayEvents[event] = true
 			} else {
+			// new day scenario
 				totalUnique += len(sameDayEvents)
 				sameDayEvents = make(map[util.Event]bool)
 				sameDayEvents[event] = true
@@ -140,6 +156,7 @@ func TotalUnique(urlPattern string, username string, password string) int {
 
 	totalUnique += len(sameDayEvents)
 	return totalUnique
+
 }
 
 func main() {
